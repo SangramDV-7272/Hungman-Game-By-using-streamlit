b08bea3d0c8b867e09b1fcb90fb48df0740463de
sangram-king <sangramvazratkar@gmail.com>
2025-02-10 19:37:22 +0530

    NA

--- dict_word.py
+++ dict_word.py
@@ -1,7 +1,9 @@
 import requests
 import json
 
+
 response = requests.get("https://api.datamuse.com/words?rel_trg=random")
+
 """[
     {"word": "chance", "score": 1000},
     {"word": "unexpected", "score": 950},
@@ -14,9 +16,6 @@ response = requests.get("https://api.datamuse.com/words?rel_trg=random")
 words = [word["word"] for word in response.json()[:10]] # print(words)  # Output: ['chance', 'unexpected', 'luck', ...]
 
 WORDS_DICT = {word: json.loads(requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}').text)[0]['meanings'][0]['definitions'][0]['definition'] for word in words if requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}').status_code == 200}
-
-
-
 """
 [
     {
